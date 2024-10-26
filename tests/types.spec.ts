6c18c461a7679c6a8b318911c6a9e0c06216bf65
Marco Pasqualetti <marco.pasqualetti@live.com>
2024-10-26 20:39:23 +0200

    test: simplify types tests

--- tests/types.spec.ts
+++ tests/types.spec.ts
@@ -1,14 +1,5 @@
-import type { RollupOptions } from 'rollup';
-
 import { oxcTransform } from '../src/index';
 
-const config: RollupOptions = {
-  input: 'main.js',
-  output: {
-    file: 'bundle.js',
-    format: 'iife',
-  },
-  plugins: [oxcTransform({})],
-};
+oxcTransform();
 
-export default config;
+oxcTransform({});
